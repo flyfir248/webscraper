6d60cfe02231074603861cc99f2e1fc49f1a5b76
@johnya0 <anoopjohny2000@gmail.com>
2023-01-09 21:49:04 +0100

    test scrape

--- main.py
+++ main.py
@@ -24,12 +24,3 @@ for book in book_containers:
     print(title, price)
 
 
-"""
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/flyfir248/webscraper.git
-git push -u origin main
-
-"""
