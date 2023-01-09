652d80ecbc62a357f6961509d1f5219c51d7c1a4
@johnya0 <anoopjohny2000@gmail.com>
2023-01-09 21:53:10 +0100

    test scrape csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 URL = "https://books.toscrape.com/"
@@ -12,18 +13,24 @@ soup = BeautifulSoup(response.text, 'html.parser')
 # Find all the book containers
 book_containers = soup.find_all('article', class_='product_pod')
 
-# Extract the data for each book
-for book in book_containers:
-    # Find the title of the book
-    title = book.h3.a.attrs['title']
+# Open a CSV file for writing
+with open('books.csv', 'w', newline='') as csvfile:
+    # Create a CSV writer
+    writer = csv.writer(csvfile)
 
-    # Find the price of the book
-    price = book.find('p', class_='price_color').text
+    # Write the header row
+    writer.writerow(['Title', 'Price', 'Link'])
 
-    # Find the link to the book
-    link = book.h3.a.attrs['href']
+    # Extract the data for each book
+    for book in book_containers:
+        # Find the title of the book
+        title = book.h3.a.attrs['title']
 
-    # Print the data for each book
-    print(title, price, link)
+        # Find the price of the book
+        price = book.find('p', class_='price_color').text
 
+        # Find the link to the book
+        link = book.h3.a.attrs['href']
 
+        # Write the data for the book to the CSV file
+        writer.writerow([title, price, link])
